c50ff0d71de436618cc77c659b4d4aa22f75b469
Pasi Junttila <pasi.junttila@live.com>
2017-12-15 15:42:57 +0200

    Some word loaded to db in test profile

--- src/main/java/base/DatabaseLoader.java
+++ src/main/java/base/DatabaseLoader.java
@@ -1,19 +1,40 @@
 package base;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import base.domain.Word;
+import base.repository.WordRepository;
 
 /**
  *
  * @author Pasi
  */
+@Profile("test")
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
+    @Autowired
+    private WordRepository wordRepository;
 
     @Override
     public void run(String... strings) throws Exception {
-
+        String [] words = {"abandon","babies","cabbage","daily","eager","fabric","gadgets","habanero","iceberg",
+                "jacket","karat","label","machete","nab","oaring","pacemaker","quadrant","rabbi","sacred","table",
+                "ubiquitous","vacation","wages","xray","yahoo","zealot"};
+        insertWords(Arrays.asList(words));
+        
     }
 
+    private void insertWords(List<String> items) {
+        for (String item: items) {
+            Word word = new Word(item);
+            wordRepository.saveAndFlush(word);
+        }
+    }
 }
--- src/main/java/base/domain/Word.java
+++ src/main/java/base/domain/Word.java
@@ -11,6 +11,13 @@ public class Word extends AbstractPersistable<Long> {
     @Column(unique=true)
     private String value;
 
+    public Word() {
+    }
+    
+    public Word(String value) {
+        this.value = value;
+    }
+    
     public String getValue() {
         return value;
     }

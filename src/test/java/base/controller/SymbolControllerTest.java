a72776d2e974d1df31f66a3187655f3b07d35c81
Pasi Junttila <pasi.junttila@live.com>
2017-12-18 12:11:18 +0200

    Symbol Ctrl add tests.

--- src/test/java/base/controller/SymbolControllerTest.java
+++ src/test/java/base/controller/SymbolControllerTest.java
@@ -1,8 +1,11 @@
 package base.controller;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -27,7 +30,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import base.domain.Symbol;
 import base.repository.SymbolRepository;
 import base.service.SymbolDto;
-import base.service.WordDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,8 +39,13 @@ public class SymbolControllerTest {
     private static final String PATH = "/api/symbols";
     private static final String SYMBOL1 = "nutrvdtfng";
     private static final String SYMBOL2 = "glp8kuirey";
+    private static final String SYMBOL3 = "jgefgvedcg";
+    private static final String EMPTY_STRING = "";
     private static final int SCORE1 = 1;
     private static final int SCORE2 = 2;
+    private static final int SCORE3 = 3;
+    private static final int ZEROSCORE = 0;
+    private static final int NEGATIVESCORE = -1;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -62,6 +69,10 @@ public class SymbolControllerTest {
     public void tearDown() throws Exception {
         symbolRepository.delete(s1);
         symbolRepository.delete(s2);
+        Symbol s3 = symbolRepository.findByValue(SYMBOL3);
+        if (null != s3) {
+            symbolRepository.delete(s3);
+        }
     }
 
     @Test
@@ -90,4 +101,73 @@ public class SymbolControllerTest {
                 .findFirst().isPresent());
     }
 
+    @Test
+    public void addingSymbolReturnsLocationHeaderAndDto() throws Exception {
+        SymbolAdd newSymbol = new SymbolAdd(SYMBOL3, SCORE3);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(newSymbol);
+        MvcResult result = mockMvc
+                                   .perform(
+                                           post(PATH)
+                                           .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                           .content(content))
+                                   .andExpect(status().isCreated())
+                                   .andExpect(header().string("Location", containsString(PATH + "/")))
+                                   .andReturn();
+        SymbolDto dto = mapper.readValue(result.getResponse().getContentAsString(), SymbolDto.class);
+        assertTrue("Didn't return correct value", dto.getValue().equals(SYMBOL3));
+        assertTrue("Didn't return correct score", dto.getScore() == SCORE3);
+    }
+    
+    @Test
+    public void addingSymbolThatExistsFails() throws Exception {
+        SymbolAdd newSymbol = new SymbolAdd(SYMBOL1, SCORE1);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(newSymbol);
+        mockMvc
+                .perform(
+                        post(PATH)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(content))
+                .andExpect(status().isLocked());
+    }
+    
+    @Test
+    public void addingSymbolWithoutValueFails() throws Exception {
+        SymbolAdd newSymbol = new SymbolAdd(EMPTY_STRING, SCORE1);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(newSymbol);
+        mockMvc
+                .perform(
+                        post(PATH)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(content))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void addingSymbolWithZeroScoreFails() throws Exception {
+        SymbolAdd newSymbol = new SymbolAdd(SYMBOL3, ZEROSCORE);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(newSymbol);
+        mockMvc
+                .perform(
+                        post(PATH)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(content))
+                .andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    public void addingSymbolWithNegativeScoreFails() throws Exception {
+        SymbolAdd newSymbol = new SymbolAdd(SYMBOL3, NEGATIVESCORE);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(newSymbol);
+        mockMvc
+                .perform(
+                        post(PATH)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .content(content))
+                .andExpect(status().isBadRequest());
+    }
 }

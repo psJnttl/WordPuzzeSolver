0e8db72f2bea247ab6280cc5584400a84009688d
Pasi Junttila <pasi.junttila@live.com>
2017-12-19 13:16:11 +0200

    SemanticUI pagination test.

--- src/main/js/components/Words.js
+++ src/main/js/components/Words.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { Menu } from 'semantic-ui-react'
 
 class Words extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {wordCount:0, itemsPerPage:10,}
+    this.state = {wordCount:0, itemsPerPage:10, activeItem: '10', }
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  handleItemClick(e, { name }) {
+    this.setState({ activeItem: name })
   }
 
   getWordCount() {
@@ -27,8 +33,17 @@ class Words extends React.Component {
   }
 
   render() {
+    const activeItem = this.state.activeItem;
     return (
-      <div>Words in database: {this.state.wordCount}
+      <div>
+        <h4>Words in database: {this.state.wordCount}</h4>
+        <Menu pagination>
+          <Menu.Item name='1' active={activeItem === '1'} onClick={this.handleItemClick} />
+          <Menu.Item disabled>...</Menu.Item>
+          <Menu.Item name='10' active={activeItem === '10'} onClick={this.handleItemClick} />
+          <Menu.Item name='11' active={activeItem === '11'} onClick={this.handleItemClick} />
+          <Menu.Item name='12' active={activeItem === '12'} onClick={this.handleItemClick} />
+        </Menu>
 
       </div>
     );

ba3088fd30c2c4e350c326dfb4fd39471c5e6c88
Pasi Junttila <pasi.junttila@live.com>
2017-12-19 12:19:27 +0200

    Fetch word count to UI.

--- src/main/js/components/Words.js
+++ src/main/js/components/Words.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class Words extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
-    this.method = this.method.bind(this);
+    this.state = {wordCount:0, itemsPerPage:10,}
   }
-  method() {}
+
+  getWordCount() {
+    const self = this;
+    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    axios.get('/api/words/count', config)
+         .then(function (response) {
+           const resp = response.data;
+           self.setState({wordCount: resp.count});
+         })
+         .catch(function (error) {
+           console.log("fetching count failed");
+         });
+
+  }
+
+  componentWillMount() {
+    this.getWordCount();
+  }
+
   render() {
     return (
-      <div>Words.</div>
+      <div>Words in database: {this.state.wordCount}
+
+      </div>
     );
   }
 }

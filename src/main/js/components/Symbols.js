acbf8d06efea19855d8401268d422acf9d2c03d5
Pasi Junttila <pasi.junttila@live.com>
2017-12-22 16:01:39 +0200

    List all symbols in db on UI.

--- src/main/js/components/Symbols.js
+++ src/main/js/components/Symbols.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { Table } from 'semantic-ui-react'
 
 class Symbols extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {symbols: [], }
 
   }
 
+  getAllSymbols() {
+    const self = this;
+    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    axios.get('/api/symbols', config)
+         .then(function (response) {
+           self.setState({symbols: response.data});
+         })
+         .catch(function (error) {
+           console.log("getting all symbols failed");
+         });
+  }
+
+  componentWillMount() {
+    this.getAllSymbols();
+  }
+
   render() {
+    const dataRows = this.state.symbols.map((item, index) =>
+       <Table.Row key={index} size="small">
+         <Table.Cell>{item.id}</Table.Cell>
+         <Table.Cell>{item.value}</Table.Cell>
+         <Table.Cell>{item.score}</Table.Cell>
+         <Table.Cell></Table.Cell>
+       </Table.Row>
+    );
     return (
-      <div>Symbols</div>
+      <div>
+        <h4>Symbols</h4>
+        <Table celled unstackable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>id</Table.HeaderCell>
+              <Table.HeaderCell>value</Table.HeaderCell>
+              <Table.HeaderCell>score</Table.HeaderCell>
+              <Table.HeaderCell>actions</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {dataRows}
+          </Table.Body>
+        </Table>
+      </div>
     );
   }
 }

400ad2251685aea61d305ef7c35210a38b047450
Pasi Junttila <pasi.junttila@live.com>
2017-12-26 17:11:38 +0200

    To mark word path in the game area.

---
+++ src/main/java/base/solver/symbol/IndexNumber.java
@@ -0,0 +1,39 @@
+package base.solver.symbol;
+/**
+ * This class is used to mark word path in the game area.
+ * @author Pasi
+ *
+ */
+public class IndexNumber implements Symbol {
+
+    private String rawPresentation;
+    private int gameAreaIndex;
+    private int points;
+
+    public IndexNumber(String letter, int gameAreaIndex, int points) {
+        this.rawPresentation = letter;
+        this.gameAreaIndex = gameAreaIndex;
+        this.points = points;
+    }
+
+    @Override
+    public int verifyNextSymbol(String nextWord, int wordIndex) {
+        return 0;
+    }
+
+    @Override
+    public int points() {
+        return points;
+    }
+
+    @Override
+    public Symbol copyOf() {
+        return null;
+    }
+
+    @Override
+    public int getCoordinates() {
+        return gameAreaIndex;
+    }
+
+}

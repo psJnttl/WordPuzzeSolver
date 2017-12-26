c09663b1e5601fbc38ba08ca6cb64f7abc671733
Pasi Junttila <pasi.junttila@live.com>
2017-12-26 13:57:19 +0200

    Letter class for single letter symbols.

---
+++ src/main/java/solver/Letter.java
@@ -0,0 +1,39 @@
+package solver;
+
+public class Letter implements Symbol {
+
+    private String rawPresentation;
+    private int gameAreaIndex;
+    private int points;
+    
+    public Letter(String letter, int gameAreaIndex, int points) {
+        rawPresentation = letter;
+        this.gameAreaIndex = gameAreaIndex;
+        this.points = points;
+    }
+
+    @Override
+    public int verifyNextSymbol(String nextWord, int wordIndex) {
+        String testWord = nextWord.substring(wordIndex);
+        int foundAt = testWord.indexOf(rawPresentation);
+        if (0 == foundAt) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public int points() {
+        return points;
+    }
+    
+    @Override
+    public String toString() {
+        return rawPresentation;
+    }
+    
+    public int getGameAreaIndex() {
+        return gameAreaIndex;
+    }
+
+}

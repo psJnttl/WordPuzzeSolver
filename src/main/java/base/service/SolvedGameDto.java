0c7178d0066b691e831aa914c2eb184f115e21da
Pasi Junttila <pasi.junttila@live.com>
2017-12-26 13:37:22 +0200

    DTO to hold solved/found words in game area.

---
+++ src/main/java/base/service/SolvedGameDto.java
@@ -0,0 +1,50 @@
+package base.service;
+
+import java.util.List;
+
+public class SolvedGameDto {
+
+    private List<SolvedWord> words;
+    
+    public SolvedGameDto() {
+    }
+    
+    public SolvedGameDto(List<SolvedWord> words) {
+        this.words = words;
+    }
+
+    public List<SolvedWord> getWords() {
+        return words;
+    }
+
+    public void setWords(List<SolvedWord> words) {
+        this.words = words;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((words == null) ? 0 : words.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        SolvedGameDto other = (SolvedGameDto) obj;
+        if (words == null) {
+            if (other.words != null) return false;
+        }
+        else if (!words.equals(other.words)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "SolvedGameDto [words=" + words + "]";
+    }
+    
+}

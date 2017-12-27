e154d18f5b75e1f349208c709c5710a5ebde7c7e
Pasi Junttila <pasi.junttila@live.com>
2017-12-27 13:14:17 +0200

    FoundWords: insert, remove and print methods.

--- src/main/java/base/solver/FoundWords.java
+++ src/main/java/base/solver/FoundWords.java
@@ -1,10 +1,46 @@
 package base.solver;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Contains a list of Words found during searchPath.
  * @author Pasi
  *
  */
 public class FoundWords {
+    private List<Word> wordList = new ArrayList<>();
 
+    public void insertWord(Word newWord) {
+        boolean duplicate = false;
+        for (Word aWord : wordList) {
+            if (aWord.toString().equals(newWord.toString())) {
+                duplicate = true; 
+                System.out.println("duplicate: " + newWord);
+                if (newWord.calculatePoints() > aWord.calculatePoints()) {
+                    removeWord(aWord); // can happen with digram, either/or:
+                    wordList.add(newWord); // same word yields more points
+                    System.out.println("replace: " + newWord);
+                }
+            }
+        }
+        if (!duplicate) {
+            wordList.add(newWord);
+        }
+    }
+    
+    public void removeWord(Word aWord) {
+        Optional<Word> tbr = wordList.stream().filter(w -> w.toString().equals(aWord)).findFirst();
+        if (tbr.isPresent()) {
+            Word toBeRemoved = tbr.get();
+            wordList.remove(toBeRemoved);
+        }
+    }
+    
+    public void printWordList() {
+        for (Word aWord : wordList) {
+            System.out.println(aWord.calculatePoints() + ": " + aWord.toString());
+        }
+    }
 }

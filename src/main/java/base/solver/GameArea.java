eee7eaccd411762ea929792c34fa336962ba79e9
Pasi Junttila <pasi.junttila@live.com>
2017-12-27 12:39:15 +0200

    Recursively go through the game are tracking word path.

--- src/main/java/base/solver/GameArea.java
+++ src/main/java/base/solver/GameArea.java
@@ -19,9 +19,10 @@ public class GameArea {
 
     @Autowired
     private WordService wordService;
-    
+
     private static final int X_SIZE = 4;
     private static final int Y_SIZE = 4;
+    private static final int MAX_WORD_LENGTH = X_SIZE * Y_SIZE;
 
     private Symbol[][] gameArea;
     private Symbol[][] tempGameArea;
@@ -29,14 +30,15 @@ public class GameArea {
     private Vocabulary vocabulary;
     private List<FoundWords> wordList;
     private Word word;
-    
+    private List<Symbol[][]> gameAreaStack;
+
     public GameArea() throws InvalidKeyException {
         this.symbolScore = new SymbolScore();
         this.vocabulary = new Vocabulary();
     }
 
     public void setGameArea(List<String> gameArea) {
-        if (null == gameArea || gameArea.isEmpty() ) {
+        if (null == gameArea || gameArea.isEmpty()) {
             throw new NullPointerException("Must have game area as String.");
         }
         else if (gameArea.size() < 16 || gameArea.size() > 16) {
@@ -50,10 +52,10 @@ public class GameArea {
         List<String> gameData = gameArea;
         this.gameArea = new Symbol[Y_SIZE][X_SIZE];
         for (int i = 0; i < Y_SIZE; i++) {
-            Symbol [] line = new Symbol[4];
+            Symbol[] line = new Symbol[4];
             for (int j = 0; j < X_SIZE; j++) {
-                Symbol symbol = SymbolFactory.create(gameData.get(i*4 + j), (i*4 + j), 1);
-                line[j] = symbol;                
+                Symbol symbol = SymbolFactory.create(gameData.get(i * 4 + j), (i * 4 + j), 1);
+                line[j] = symbol;
             }
             this.gameArea[i] = line;
         }
@@ -70,39 +72,93 @@ public class GameArea {
         }
         return sb.toString();
     }
-    
+
     public void solve() {
         wordList = new ArrayList<>();
         for (int i = 0; i < Y_SIZE; i++) {
             for (int j = 0; j < X_SIZE; j++) {
                 Symbol symbol = gameArea[i][j];
-                vocabulary.useSubVocabulary(symbol); 
+                vocabulary.useSubVocabulary(symbol);
                 processVocabulary(symbol, i, j);
             }
         }
     }
-    
+
     private void processVocabulary(Symbol symbol, int gY, int gX) {
         String nextWord = "";
         while (true) {
-            Optional<String> nextWordConditional = vocabulary.getNextWord(); 
+            Optional<String> nextWordConditional = vocabulary.getNextWord();
             if (nextWordConditional.isPresent()) {
                 nextWord = nextWordConditional.get();
-                backUpGameArea();  // mutating will take place
-                word = new Word(nextWord);  // track word path
+                this.tempGameArea = backUpGameArea(this.gameArea); // mutating
+                word = new Word(nextWord); // track word path
+                gameAreaStack = new ArrayList<>();
+                searchPath(nextWord, 0, gY, gX);
             }
             else {
                 break;
             }
         }
     }
-    
-    private void backUpGameArea() {
-        this.tempGameArea = new Symbol[Y_SIZE][X_SIZE];
+
+    private Symbol[][] backUpGameArea(Symbol[][] gameTiles) {
+        Symbol[][] tempGameArea = new Symbol[Y_SIZE][X_SIZE];
         for (int i = 0; i < Y_SIZE; i++) {
             for (int j = 0; j < X_SIZE; j++) {
-                tempGameArea[i][j] = gameArea[i][j].copyOf();
+                tempGameArea[i][j] = gameTiles[i][j].copyOf();
             }
         }
+        return tempGameArea;
+    }
+
+    private void searchPath(String nextWord, int wordIndex, int gY, int gX) {
+        if (gY >= Y_SIZE || gY < 0 || gX >= X_SIZE || gX < 0 || wordIndex >= MAX_WORD_LENGTH) {
+            return;
+        }
+        Symbol tileSymbol = tempGameArea[gY][gX];
+        int status = tileSymbol.verifyNextSymbol(nextWord, wordIndex);
+        if (status > 0) {
+            word.insertSymbol(tileSymbol);
+            if (nextWord.length() == (wordIndex + status)) {
+                word.setComplete();
+                return;
+            }
+            else {
+                Symbol order = SymbolFactory.create(Integer.toString(wordIndex), 0, 0);
+                this.gameAreaStack.add(backUpGameArea(this.tempGameArea)); // for
+                                                                           // backtracking
+                this.tempGameArea[gY][gX] = order;
+                wordIndex += status;
+            }
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY, gX + 1); // E
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY + 1, gX + 1); // SE
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY + 1, gX); // S
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY + 1, gX - 1); // SW
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY, gX - 1); // W
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY - 1, gX - 1); // NW
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY - 1, gX); // N
+        }
+        if (!word.isComplete()) {
+            searchPath(nextWord, wordIndex, gY - 1, gX + 1); // NE
+        }
+        if (!word.isComplete()) { // nowhere to go, word not found
+            this.tempGameArea = gameAreaStack.remove(gameAreaStack.size() - 1);
+            this.word.removeLastSymbol();
+        }
+
     }
 }

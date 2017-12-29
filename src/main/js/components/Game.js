0c9e71ec9d20bee83a09f448e5a67666f35eac2d
Pasi Junttila <pasi.junttila@live.com>
2017-12-29 14:18:18 +0200

    GameTiles to grid

--- src/main/js/components/Game.js
+++ src/main/js/components/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Input } from 'semantic-ui-react'
 import _ from 'lodash';
+import GameTile from './GameTile';
 
 class Game extends React.Component {
   constructor(props) {
@@ -9,46 +10,140 @@ class Game extends React.Component {
     this.state = {
       tileValues: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'],
     };
-    this.onChangeTileValue0 = this.onChangeTileValue0.bind(this);
+    this.onChangeTileValue = this.onChangeTileValue.bind(this);
   }
 
-  onChangeTileValue0(e) {
-    const value = e.target.value;
+  onChangeTileValue(e, index) {
     var array = this.state.tileValues;
-    array[0] = value;
-    const poista = _.take(array, 16);
-    this.setState({tileValues: poista})
+    array[index] = e.target.value;
+    this.setState({tileValues: _.take(array, 16)})
   }
+
   render() {
+
     return (
       <div>
         <h4>Game</h4>
         <Grid columns={4}>
-          <Grid.Row celled>
+          <Grid.Row>
             <Grid.Column>
-              
+              <GameTile
+                value={this.state.tileValues[0]}
+                onChange={this.onChangeTileValue}
+                index={0}
+              />
             </Grid.Column>
-            <Grid.Column>2</Grid.Column>
-            <Grid.Column>3</Grid.Column>
-            <Grid.Column>4</Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[1]}
+                onChange={this.onChangeTileValue}
+                index={1}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[2]}
+                onChange={this.onChangeTileValue}
+                index={2}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[3]}
+                onChange={this.onChangeTileValue}
+                index={3}
+              />
+            </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column>5</Grid.Column>
-            <Grid.Column>6</Grid.Column>
-            <Grid.Column>7</Grid.Column>
-            <Grid.Column>8</Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[4]}
+                onChange={this.onChangeTileValue}
+                index={4}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[5]}
+                onChange={this.onChangeTileValue}
+                index={5}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[6]}
+                onChange={this.onChangeTileValue}
+                index={6}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[7]}
+                onChange={this.onChangeTileValue}
+                index={7}
+              />
+            </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column>9</Grid.Column>
-            <Grid.Column>10</Grid.Column>
-            <Grid.Column>11</Grid.Column>
-            <Grid.Column>12</Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[8]}
+                onChange={this.onChangeTileValue}
+                index={8}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[9]}
+                onChange={this.onChangeTileValue}
+                index={9}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[10]}
+                onChange={this.onChangeTileValue}
+                index={10}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[11]}
+                onChange={this.onChangeTileValue}
+                index={11}
+              />
+            </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column>13</Grid.Column>
-            <Grid.Column>14</Grid.Column>
-            <Grid.Column>15</Grid.Column>
-            <Grid.Column>16</Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[12]}
+                onChange={this.onChangeTileValue}
+                index={12}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[13]}
+                onChange={this.onChangeTileValue}
+                index={13}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[14]}
+                onChange={this.onChangeTileValue}
+                index={14}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <GameTile
+                value={this.state.tileValues[15]}
+                onChange={this.onChangeTileValue}
+                index={15}
+              />
+            </Grid.Column>
           </Grid.Row>
         </Grid>
       </div>

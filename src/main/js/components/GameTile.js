37ca74a48bce6e89c274771bb167c2f88252a419
Pasi Junttila <pasi.junttila@live.com>
2017-12-29 14:17:56 +0200

    GameTile component

---
+++ src/main/js/components/GameTile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GameTile = (props) => (
+  <div>
+    <input
+      value={props.value}
+      onChange={ (e) => props.onChange(e, props.index)}
+      autoComplete="off"
+      size={2}
+    />
+  </div>
+);
+GameTile.PropTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+}
+GameTile.defaultProps = {}
+export default GameTile;

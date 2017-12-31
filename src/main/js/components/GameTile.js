e988d7d0450a04118ad3c62df0fc752ea574900e
Pasi Junttila <pasi.junttila@live.com>
2017-12-31 14:48:21 +0200

    GameTile style parametrization.

--- src/main/js/components/GameTile.js
+++ src/main/js/components/GameTile.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
-const GameTile = (props) => (
-  <div>
-    <input
-      value={props.value}
-      onChange={ (e) => props.onChange(e, props.index)}
-      autoComplete="off"
-      size={2}
-      className="gametileinput"
-    />
-  </div>
-);
+class GameTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputStyle : {
+        fontFamily: "Lato,'Helvetica Neue',Arial,Helvetica,sans-serif",
+        fontSize: "32px",
+        border: "5px solid rgba(34,36,38,.15)",
+        textAlign: "center" },
+    };
+
+  }
+
+  render() {
+    const inputStyle = _.assign({}, this.state.inputStyle, this.props.background);
+    return (
+      <div>
+        <input
+          value={this.props.value}
+          onChange={ (e) => this.props.onChange(e, this.props.index)}
+          autoComplete="off"
+          size={2}
+          style={inputStyle}
+        />
+      </div>
+    );
+  }
+}
+
 GameTile.PropTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
+  background: PropTypes.object,
 }
-GameTile.defaultProps = {}
+GameTile.defaultProps = {
+  background: {background: "#ffffff"}
+}
 export default GameTile;

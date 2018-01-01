b362b3f199fcdf8ba5e14c0ae898febcb594a3e1
Pasi Junttila <pasi.junttila@live.com>
2018-01-01 13:36:48 +0200

    Default colors for word path tracking.

---
+++ src/main/js/components/DefaultColors.js
@@ -0,0 +1,18 @@
+export default  [
+  {red: 255, green: 255, blue: 255, alpha: 1},
+  {red: 245, green: 255, blue: 255, alpha: 1},
+  {red: 235, green: 255, blue: 255, alpha: 1},
+  {red: 225, green: 255, blue: 255, alpha: 1},
+  {red: 215, green: 255, blue: 255, alpha: 1},
+  {red: 205, green: 255, blue: 255, alpha: 1},
+  {red: 195, green: 255, blue: 255, alpha: 1},
+  {red: 185, green: 255, blue: 255, alpha: 1},
+  {red: 175, green: 255, blue: 255, alpha: 1},
+  {red: 165, green: 255, blue: 255, alpha: 1},
+  {red: 155, green: 255, blue: 255, alpha: 1},
+  {red: 145, green: 255, blue: 255, alpha: 1},
+  {red: 135, green: 255, blue: 255, alpha: 1},
+  {red: 125, green: 255, blue: 255, alpha: 1},
+  {red: 115, green: 255, blue: 255, alpha: 1},
+  {red: 105, green: 255, blue: 255, alpha: 1}
+];

8a14d9654825abe404a1277421b839e0fe0f07dc
Pasi Junttila <pasi.junttila@live.com>
2018-01-03 17:36:16 +0200

    Default tile colors loaded to test repository.

--- src/main/java/base/DatabaseLoader.java
+++ src/main/java/base/DatabaseLoader.java
@@ -10,8 +10,10 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import base.domain.Symbol;
+import base.domain.TileColor;
 import base.domain.Word;
 import base.repository.SymbolRepository;
+import base.repository.TileColorRepository;
 import base.repository.WordRepository;
 
 /**
@@ -27,6 +29,9 @@ public class DatabaseLoader implements CommandLineRunner {
     
     @Autowired
     private SymbolRepository symbolRepository;
+    
+    @Autowired
+    private TileColorRepository tileColorRepository;
 
     @Override
     public void run(String... strings) throws InvalidKeyException {
@@ -36,9 +41,28 @@ public class DatabaseLoader implements CommandLineRunner {
         final Object[] symbolScores = { "a", 2, "at", 6, "b", 5, "c", 3, "d", 3, "e", 1, "ed", 7, "-est", 12,
                 "f", 5, "g", 4, "h", 4, "i", 2, "i/m", 20, "in-", 12, "j", 10, "k", 6, "l", 3, "ll", 8, "m", 4, "n", 2, "o",
                 2, "p", 4, "q", 10, "r", 2, "s", 2, "t", 2, "u", 4, "u/f", 20, "v", 6, "w", 6, "x", 9, "y", 5, "z", 8 };
-
+        final int [][] colors = {
+                {227, 242, 253},
+                {217, 232, 252},
+                {187, 222, 251},
+                {165, 212, 250},
+                {144, 202, 249},
+                {122, 191, 248},
+                {100, 181, 246},
+                { 88, 173, 245},
+                { 66, 165, 245},
+                { 49, 158, 244},
+                { 33, 150, 243},
+                { 32, 143, 236},
+                { 30, 136, 229},
+                { 27, 127, 219},
+                { 25, 118, 210},
+                { 22, 109, 200}
+        };
+        final double [] alpha = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
         insertWords(Arrays.asList(words));
         insertSymbolScores(symbolScores);
+        insertTileColors(colors, alpha);
     }
 
     private void insertWords(List<String> items) {
@@ -62,7 +86,15 @@ public class DatabaseLoader implements CommandLineRunner {
                 Symbol symbol = new Symbol(value, score);
                 symbol = symbolRepository.saveAndFlush(symbol);
             }
+        }    
+    }
+    
+    private void insertTileColors(int[][] colors, double[] alpha) {
+        for (int i = 0; i < colors.length; i++) {
+            TileColor color = new TileColor(colors[i][0],colors[i][1],colors[i][2],alpha[i]);
+            tileColorRepository.saveAndFlush(color);
         }
         
     }
+
 }

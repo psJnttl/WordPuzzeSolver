3e638bc633944009ab2d150530a418a9631b1f96
Pasi Junttila <pasi.junttila@live.com>
2018-01-04 13:19:34 +0200

    Colors loaded to component from server.

--- src/main/js/components/Colors.js
+++ src/main/js/components/Colors.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class Colors extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {colors: [], }
+  }
+
+  loadColors() {
+    const self = this;
+    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    axios.get('/api/colors/', config)
+         .then(function (response) {
+           self.setState({colors: response.data});
+         })
+         .catch(function (error) {
+           console.log("Failed to load colors from server.");
+         });
+  }
+
+  componentWillMount() {
+    this.loadColors();
   }
 
   render() {

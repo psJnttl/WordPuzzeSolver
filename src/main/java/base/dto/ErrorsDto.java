08c726f34215298873bc6653389a2415f72dbaf6
Pasi Junttila <pasi.junttila@live.com>
2018-01-10 15:13:40 +0200

    Dto to hold a list of messages.

---
+++ src/main/java/base/dto/ErrorsDto.java
@@ -0,0 +1,57 @@
+package base.dto;
+
+import java.util.List;
+
+public class ErrorsDto {
+
+    private String itemType;
+    private List<String> messages;
+    
+    public ErrorsDto(String itemType, List<String> messages) {
+        this.itemType = itemType;
+        this.messages = messages;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<String> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((messages == null) ? 0 : messages.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        ErrorsDto other = (ErrorsDto) obj;
+        if (messages == null) {
+            if (other.messages != null) return false;
+        }
+        else if (!messages.equals(other.messages)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorsDto [messages=" + messages + "]";
+    }
+    
+}
